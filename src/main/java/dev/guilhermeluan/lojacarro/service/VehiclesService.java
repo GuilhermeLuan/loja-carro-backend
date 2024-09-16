946ff20b9b3b09506b0fdf146357a25275900d3a
Guilherme Luan <guilhermeluan@pm.me>
2024-09-16 19:31:48 -0300

    feat: create update and delete on service

--- src/main/java/dev/guilhermeluan/lojacarro/service/VehiclesService.java
+++ src/main/java/dev/guilhermeluan/lojacarro/service/VehiclesService.java
@@ -1,6 +1,5 @@
 package dev.guilhermeluan.lojacarro.service;
 
-import dev.guilhermeluan.lojacarro.dtos.request.VehiclesPostRequest;
 import dev.guilhermeluan.lojacarro.exception.BadRequestException;
 import dev.guilhermeluan.lojacarro.mapper.VehiclesMapper;
 import dev.guilhermeluan.lojacarro.model.Vehicles;
@@ -14,18 +13,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class VehiclesService {
     private static final VehiclesMapper MAPPPER = VehiclesMapper.INSTANCE;
-    private final VehiclesRepository vehiclesRepository;
+    private final VehiclesRepository repository;
 
     public Page<Vehicles> findALl(Pageable pageable, String param) {
-        return param == null ? vehiclesRepository.findAll(pageable) : vehiclesRepository.findByModelIgnoreCase(param, pageable);
+        return param == null ? repository.findAll(pageable) : repository.findByModelIgnoreCase(param, pageable);
     }
 
     public Vehicles save(Vehicles vehicle) {
-        return vehiclesRepository.save(vehicle);
+        return repository.save(vehicle);
     }
 
     public Vehicles findByIdOrThrowBadRequestException(Long id) {
-        return vehiclesRepository.findById(id).
+        return repository.findById(id).
                 orElseThrow(() -> new BadRequestException("Vehicle not found"));
+    }
+
+    public void deleteById(Long id){
+        var vehicles = repository.findById(id);
+        repository.deleteById(id);
+    }
+
+    public void update(Vehicles vehicle) {
+        assertVehicleExist(vehicle.getId());
+
+        repository.deleteById(vehicle.getId());
+        repository.save(vehicle);
+    }
+
+    private void assertVehicleExist(Long id) {
+        findByIdOrThrowBadRequestException(id);
     }
 }
