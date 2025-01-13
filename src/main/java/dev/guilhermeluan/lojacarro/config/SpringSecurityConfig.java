2408d2c134ef0602eef8c2d365e709f9285d3bcd
Guilherme Luan <guilhermeluan@pm.me>
2025-01-13 19:58:20 -0300

    feat: security endpoint for post, put, delete http methods

---
+++ src/main/java/dev/guilhermeluan/lojacarro/config/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.guilhermeluan.lojacarro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+    private static final String[] WHITE_LIST = {"/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/csrf"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/vehicles/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/vehicles").permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
